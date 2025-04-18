bd8086b923795fba21d65dc7a95422386d78cbb0
dnikashov <nikashov.daniel@gmail.com>
2025-04-18 01:21:00 -0400

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop"; 
 
 import Home from "./components/Home";
